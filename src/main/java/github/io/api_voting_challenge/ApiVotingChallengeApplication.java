654631714f8a7869d93e7526161dad7b4c49868d
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-08-04 10:52:02 -0300

    first commit

---
+++ src/main/java/github/io/api_voting_challenge/ApiVotingChallengeApplication.java
@@ -0,0 +1,13 @@
+package github.io.api_voting_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVotingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVotingChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/io/api_voting_challenge/ApiVotingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package github.io.api_voting_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVotingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
