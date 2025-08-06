6e4016eebd017a2d5119028cbd30d6a78b9e65f0
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-08-06 10:30:49 -0300

    feat: add User model for user management and authentication

---
+++ src/main/java/github/io/api_voting_challenge/model/User.java
@@ -0,0 +1,26 @@
+package github.io.api_voting_challenge.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String cpf;
+    private String email;
+    private String password;
+    private Role role;
+}
