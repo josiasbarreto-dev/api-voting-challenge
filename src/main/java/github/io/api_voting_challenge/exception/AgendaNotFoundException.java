e0397c543b34cef84125d6d7292d338a69db57c9
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-08-06 10:05:20 -0300

    feat: add AgendaNotFoundException for handling agenda-related errors

---
+++ src/main/java/github/io/api_voting_challenge/exception/AgendaNotFoundException.java
@@ -0,0 +1,7 @@
+package github.io.api_voting_challenge.exception;
+
+public class AgendaNotFoundException extends RuntimeException{
+    public AgendaNotFoundException(String message) {
+        super(message);
+    }
+}
