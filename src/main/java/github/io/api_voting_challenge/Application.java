ac771ee3167dbec318ee894ec164f5c96cdbb10f
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-08-06 10:20:17 -0300

    feat: load environment variables using dotenv-java in the main application

--- src/main/java/github/io/api_voting_challenge/Application.java
+++ src/main/java/github/io/api_voting_challenge/Application.java
@@ -1,5 +1,6 @@
 package github.io.api_voting_challenge;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(Application.class, args);
 	}
-
 }
