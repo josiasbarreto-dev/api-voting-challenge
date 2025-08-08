102f4d18a45c6f20c6040446c7f6135938cfc310
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-08-08 10:27:47 -0300

    feat: enable scheduling support in the application

--- src/main/java/github/io/api_voting_challenge/Application.java
+++ src/main/java/github/io/api_voting_challenge/Application.java
@@ -3,8 +3,10 @@ package github.io.api_voting_challenge;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
