2fb8fdbe88918c4b807290ba3eb4725d00f31d27
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-08-08 12:23:09 -0300

    reafctor: Change the repository name to "UserAdminRepository" and change the return value of the custom methods to return an optional AdminUser.

--- src/main/java/github/io/api_voting_challenge/repository/UserAdminRepository.java
+++ src/main/java/github/io/api_voting_challenge/repository/UserAdminRepository.java
@@ -1,14 +1,14 @@
 package github.io.api_voting_challenge.repository;
 
-import github.io.api_voting_challenge.model.User;
+import github.io.api_voting_challenge.model.AdminUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+public interface UserAdminRepository extends JpaRepository<AdminUser, Long> {
+    Optional<AdminUser> findByEmail(String email);
     boolean existsByCpf(String cpf);
-    Optional<User> findByCpf(String cpf);
+    Optional<AdminUser> findByCpf(String cpf);
 }
