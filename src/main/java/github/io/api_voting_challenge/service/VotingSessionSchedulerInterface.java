670fbd11092c0e79a1dd962de07d8b91d1549a1e
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-08-21 07:19:41 -0300

    feat: adiciona suporte a paginação para listagem de VotingSession.

--- src/main/java/github/io/api_voting_challenge/service/VotingSessionSchedulerInterface.java
+++ src/main/java/github/io/api_voting_challenge/service/VotingSessionSchedulerInterface.java
@@ -1,10 +1,10 @@
 package github.io.api_voting_challenge.service;
 
 import github.io.api_voting_challenge.dto.VotingSessionResponseDto;
-
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface VotingSessionSchedulerInterface {
-    List<VotingSessionResponseDto> getOpenVotingSessions();
+    Page<VotingSessionResponseDto> getOpenVotingSessions(Pageable pageable);
     void checkExpiredVotingSessions();
 }
