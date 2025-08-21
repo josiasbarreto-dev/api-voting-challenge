4cdd2c331dd227dd4125349c750835fc7225b8cf
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-08-21 08:08:40 -0300

    refactor: optimizes VoteService creation with the Builder pattern.

--- src/main/java/github/io/api_voting_challenge/service/impl/VoteServiceImpl.java
+++ src/main/java/github/io/api_voting_challenge/service/impl/VoteServiceImpl.java
@@ -12,6 +12,7 @@ import github.io.api_voting_challenge.repository.UserVotingRepository;
 import github.io.api_voting_challenge.repository.VoteRepository;
 import github.io.api_voting_challenge.repository.VotingSessionRepository;
 import github.io.api_voting_challenge.service.VoteServiceInterface;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,21 +20,16 @@ import java.time.LocalDateTime;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class VoteServiceImpl implements VoteServiceInterface {
     private final UserVotingRepository userVotingRepository;
     private final VotingSessionRepository votingSessionRepository;
     private final VoteRepository voteRepository;
 
-    public VoteServiceImpl(UserVotingRepository userVotingRepository, VotingSessionRepository votingSessionRepository, VoteRepository voteRepository) {
-        this.userVotingRepository = userVotingRepository;
-        this.votingSessionRepository = votingSessionRepository;
-        this.voteRepository = voteRepository;
-    }
-
     @Override
     public void registerVote(Long sessionId, Long userId, VoteRequestDTO voteRequest) {
         VotingUser user = userVotingRepository.findById(userId).orElseThrow(
-                        () -> new UserNotFoundException("User not found with ID: " + userId));
+                () -> new UserNotFoundException("User not found with ID: " + userId));
         VotingSession session = votingSessionRepository.findById(sessionId).orElseThrow(
                 () -> new VotingSessionNotFoundException("Voting session not found with ID: " + sessionId));
 
@@ -46,10 +42,11 @@ public class VoteServiceImpl implements VoteServiceInterface {
             throw new UserAlreadyVotedException("User has already voted in this session.");
         }
 
-        Vote vote = new Vote();
-        vote.setUser(user);
-        vote.setAgenda(session.getAgenda());
-        vote.setVoteOption(voteRequest.voteOption());
+        Vote vote = Vote.builder()
+                .user(user)
+                .agenda(session.getAgenda())
+                .voteOption(voteRequest.voteOption())
+                .build();
 
         voteRepository.save(vote);
     }
