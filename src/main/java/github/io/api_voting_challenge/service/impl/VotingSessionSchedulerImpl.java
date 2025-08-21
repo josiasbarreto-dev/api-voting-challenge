8d92ae709816f038810c548c5bbc4bb16a2ec425
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-08-21 07:28:52 -0300

    refactor: remove constructor boilerplate with @RequiredArgsConstructor.

--- src/main/java/github/io/api_voting_challenge/service/impl/VotingSessionSchedulerImpl.java
+++ src/main/java/github/io/api_voting_challenge/service/impl/VotingSessionSchedulerImpl.java
@@ -8,24 +8,21 @@ import github.io.api_voting_challenge.model.enums.Status;
 import github.io.api_voting_challenge.repository.AgendaRepository;
 import github.io.api_voting_challenge.repository.VotingSessionRepository;
 import github.io.api_voting_challenge.service.VotingSessionSchedulerInterface;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
+@RequiredArgsConstructor
 public class VotingSessionSchedulerImpl implements VotingSessionSchedulerInterface {
     private final AgendaRepository agendaRepository;
     private final VotingSessionRepository votingSessionRepository;
     private final VotingSessionMapper votingSessionMapper;
-
-    public VotingSessionSchedulerImpl(AgendaRepository agendaRepository, VotingSessionRepository votingSessionRepository, VotingSessionMapper votingSessionMapper) {
-        this.agendaRepository = agendaRepository;
-        this.votingSessionRepository = votingSessionRepository;
-        this.votingSessionMapper = votingSessionMapper;
-    }
 
     @Override
     @Scheduled(fixedRate = 60000)
@@ -43,11 +40,8 @@ public class VotingSessionSchedulerImpl implements VotingSessionSchedulerInterfa
     }
 
     @Override
-    public List<VotingSessionResponseDto> getOpenVotingSessions() {
-        List<VotingSession> openSessions = votingSessionRepository.findByEndTimeAfter(LocalDateTime.now());
-
-        return openSessions.stream()
-                .map(votingSessionMapper::toDto)
-                .collect(Collectors.toList());
+    public Page<VotingSessionResponseDto> getOpenVotingSessions(Pageable pageable) {
+        return votingSessionRepository.findByEndTimeAfter(LocalDateTime.now(), pageable)
+                .map(votingSessionMapper::toDto);
     }
 }
