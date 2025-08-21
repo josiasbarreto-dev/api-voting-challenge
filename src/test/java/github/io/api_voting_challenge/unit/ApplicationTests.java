fd65378294c8486ae038b4f7acf45fa01f6a19d5
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-08-21 08:30:17 -0300

    test: default.

---
+++ src/test/java/github/io/api_voting_challenge/unit/ApplicationTests.java
@@ -0,0 +1,13 @@
+package github.io.api_voting_challenge.unit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
