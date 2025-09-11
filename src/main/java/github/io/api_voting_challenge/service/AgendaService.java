d8bd341248a0f54a06face8f764bdd17f2aed20e
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-09-11 19:06:18 -0300

    feat: Adds new CRUD methods to the 'AgendaService' interface.

--- src/main/java/github/io/api_voting_challenge/service/AgendaService.java
+++ src/main/java/github/io/api_voting_challenge/service/AgendaService.java
@@ -2,7 +2,13 @@ package github.io.api_voting_challenge.service;
 
 import github.io.api_voting_challenge.dto.AgendaRequest;
 import github.io.api_voting_challenge.dto.AgendaResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface AgendaService {
-    AgendaResponse createAgenda(AgendaRequest agendaRequest);
+    AgendaResponse create(AgendaRequest agendaRequest);
+    AgendaResponse update(Long id, AgendaRequest agendaRequest);
+    AgendaResponse getById(Long id);
+    Page<AgendaResponse> getAll(Pageable pageable);
+    void delete(Long id);
 }
