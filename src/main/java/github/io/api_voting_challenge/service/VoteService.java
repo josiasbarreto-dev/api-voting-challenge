21e25e1ab80e51a8e0fb0151afccc3bffd6a4a05
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-09-11 19:09:00 -0300

    refactor: Removes the 'calculateVotingResult' method from the 'VoteService' interface.

--- src/main/java/github/io/api_voting_challenge/service/VoteService.java
+++ src/main/java/github/io/api_voting_challenge/service/VoteService.java
@@ -1,9 +1,7 @@
 package github.io.api_voting_challenge.service;
 
 import github.io.api_voting_challenge.dto.VoteRequest;
-import github.io.api_voting_challenge.dto.VoteResultResponse;
 
 public interface VoteService {
-    void registerVote(Long sessionId, Long userId, VoteRequest voteRequest);
-    VoteResultResponse calculateVotingResult(Long sessionId);
+    void registerVote(Long sessionId, VoteRequest voteRequest);
 }
