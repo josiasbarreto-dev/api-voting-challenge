a6e78ba218c50565eaa0af2f2e3ac277121f45e6
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-09-11 16:24:09 -0300

    refactor: Adds the 'calculateVotingResult' method to the VotingSession interface.

--- src/main/java/github/io/api_voting_challenge/service/VotingSessionService.java
+++ src/main/java/github/io/api_voting_challenge/service/VotingSessionService.java
@@ -1,8 +1,10 @@
 package github.io.api_voting_challenge.service;
 
+import github.io.api_voting_challenge.dto.VoteResultResponse;
 import github.io.api_voting_challenge.dto.VotingSessionRequest;
 import github.io.api_voting_challenge.dto.VotingSessionResponse;
 
 public interface VotingSessionService {
     VotingSessionResponse openVotingSession(VotingSessionRequest votingSessionRequest);
+    VoteResultResponse calculateVotingResult(Long sessionId);
 }
