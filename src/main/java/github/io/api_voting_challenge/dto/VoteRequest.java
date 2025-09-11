12cd87e8738112819dd8bdc926c28c6c9a891ef5
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-09-11 16:28:14 -0300

    refactor: Adds the 'userId' attribute to the 'VoteRequest' dto.

--- src/main/java/github/io/api_voting_challenge/dto/VoteRequest.java
+++ src/main/java/github/io/api_voting_challenge/dto/VoteRequest.java
@@ -1,11 +1,19 @@
 package github.io.api_voting_challenge.dto;
 
 import github.io.api_voting_challenge.model.enums.VoteOption;
+import io.swagger.v3.oas.annotations.Parameter;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Builder
 public record VoteRequest(
+        @NotNull(message = "User ID cannot be null.")
+        Long userId,
+
         @NotNull(message = "Vote option cannot be null.")
         VoteOption voteOption
 ){}
