9483762788a00266c8f8516641e39527e2fd7889
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-09-11 19:02:38 -0300

    Refactor: Modifies MapStruct conversions to return the User entity instead of AdminUser.

--- src/main/java/github/io/api_voting_challenge/mapper/UserMapper.java
+++ src/main/java/github/io/api_voting_challenge/mapper/UserMapper.java
@@ -1,25 +1,15 @@
 package github.io.api_voting_challenge.mapper;
 
-import github.io.api_voting_challenge.dto.AdminUserRequest;
-import github.io.api_voting_challenge.dto.AdminUserResponse;
-import github.io.api_voting_challenge.dto.VoterRequest;
-import github.io.api_voting_challenge.dto.VoterResponse;
-import github.io.api_voting_challenge.model.AdminUser;
-import github.io.api_voting_challenge.model.VotingUser;
+import github.io.api_voting_challenge.dto.UserRequest;
+import github.io.api_voting_challenge.dto.UserResponse;
+import github.io.api_voting_challenge.model.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    AdminUserResponse toDto(AdminUser adminUser);
+    UserResponse toDto(User user);
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "role", ignore = true)
-    AdminUser toEntity(AdminUserRequest adminUserRequest);
-
-    VoterResponse toDto(VotingUser votingUser);
-
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "role", ignore = true)
-    VotingUser toEntity(VoterRequest voterRequest);
+    User toEntity(UserRequest userRequest);
 }
