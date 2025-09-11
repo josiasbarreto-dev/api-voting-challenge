6640b1a70a1e316386d2c8232809acfd204f1201
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-09-11 16:31:57 -0300

    refactor: Change the table name to 'tb_users', change the annotation to '@builder' due to eliminating the concept of inheritance and eliminate the 'role' attribute.

--- src/main/java/github/io/api_voting_challenge/model/User.java
+++ src/main/java/github/io/api_voting_challenge/model/User.java
@@ -1,26 +1,22 @@
 package github.io.api_voting_challenge.model;
 
-import github.io.api_voting_challenge.model.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 @Entity
-@Table(name = "voting_users")
+@Table(name = "tb_users")
 @Inheritance(strategy = InheritanceType.JOINED)
 @Getter
 @Setter
-@SuperBuilder
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class VotingUser {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     @Column(unique = true, nullable = false)
     private String cpf;
-
-    @Enumerated(EnumType.STRING)
-    private Role role;
 }
