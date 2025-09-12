6b5e946ab2f0d31da678808564f6c0389bbecac8
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-09-12 01:00:12 -0300

    feat: Adds new methods to the Pauta controller and implements the swagger documentation interface 'AgendaControllerDocs'.

--- src/main/java/github/io/api_voting_challenge/controller/AgendaController.java
+++ src/main/java/github/io/api_voting_challenge/controller/AgendaController.java
@@ -1,15 +1,13 @@
 package github.io.api_voting_challenge.controller;
 
+import github.io.api_voting_challenge.controller.docs.AgendaControllerDocs;
 import github.io.api_voting_challenge.dto.AgendaRequest;
 import github.io.api_voting_challenge.dto.AgendaResponse;
 import github.io.api_voting_challenge.service.AgendaService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,14 +15,37 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/agendas")
-public class AgendaController {
-    private AgendaService agendaService;
+public class AgendaController implements AgendaControllerDocs {
+    private final AgendaService agendaService;
 
-    @Operation(summary = "Create a new agenda for voting", description = "Allows an admin to create a new agenda. The agenda is initially in an inactive state.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Agenda created successfully", content = @Content(schema = @Schema(implementation = AgendaResponse.class)))})
+    @Override
     @PostMapping
-    public ResponseEntity<AgendaResponse> createAgenda(@RequestBody @Valid AgendaRequest agendaRequest) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(agendaService.createAgenda(agendaRequest));
+    public ResponseEntity<AgendaResponse> create(@RequestBody @Valid AgendaRequest agendaRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(agendaService.create(agendaRequest));
+    }
+
+    @Override
+    @PutMapping("/{id}")
+    public ResponseEntity<AgendaResponse> update(@PathVariable Long id, @RequestBody @Valid AgendaRequest agendaRequest) {
+        return ResponseEntity.ok(agendaService.update(id, agendaRequest));
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    public ResponseEntity<AgendaResponse> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(agendaService.getById(id));
+    }
+
+    @Override
+    @GetMapping
+    public ResponseEntity<Page<AgendaResponse>> getAll(Pageable pageable) {
+        return ResponseEntity.ok(agendaService.getAll(pageable));
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        agendaService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
