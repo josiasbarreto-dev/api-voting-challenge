91766c2e6d3f3e9a6c7af5f9635ca2c64611e621
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-09-16 06:36:47 -0300

    refactor(test): change the name of the constants to user instead of the previous userAdmin.

--- src/test/java/github/io/api_voting_challenge/fixtures/TestConstants.java
+++ src/test/java/github/io/api_voting_challenge/fixtures/TestConstants.java
@@ -9,22 +9,17 @@ public class TestConstants {
     public static final Long VALID_USER_ID = 1L;
 
     public static final String VALID_CPF = "130.399.160-84";
+    public static final String VALID_CPF_UPDATE = "528.375.080-98";
     public static final String INVALID_CPF = "123.456.789-10";
 
-    public static final String VALID_EMAIL = "email@test.com";
-    public static final String INVALID_EMAIL = "test.com";
-
-    public static final String VALID_NAME = "Admin Test";
-    public static final String INVALID_NAME = "admin Test";
-    public static final String UPDATED_NAME = "Admin Updated";
-
-    public static final String VALID_PASSWORD = "Password123";
-    public static final String UPDATED_PASSWORD = "NewPassword123";
+    public static final String VALID_NAME = "User Test";
+    public static final String INVALID_NAME = "user Test";
+    public static final String UPDATED_NAME = "User Updated";
 
     public static final String VALID_AGENDA_TITLE = "title test";
     public static final String VALID_AGENDA_DESCRIPTION = "description test";
 
-    public static final String MESSAGE_ADMIN_NOT_FOUND = "Admin user not found";
+    public static final String MESSAGE_ADMIN_NOT_FOUND = "User not found";
 
     private TestConstants() {}
 }
