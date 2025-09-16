5176cfeb0f1654decb8b71f1bcccc6fc52b42ba9
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-09-16 06:57:15 -0300

    refactor: modifies the voting fixtures, adds the 'userId' attribute to the vote request.

--- src/test/java/github/io/api_voting_challenge/fixtures/VoteFixtures.java
+++ src/test/java/github/io/api_voting_challenge/fixtures/VoteFixtures.java
@@ -5,16 +5,19 @@ import github.io.api_voting_challenge.dto.VoteResultResponse;
 import github.io.api_voting_challenge.model.Vote;
 import github.io.api_voting_challenge.model.enums.VoteOption;
 
+import static github.io.api_voting_challenge.fixtures.TestConstants.INVALID_ID;
 import static github.io.api_voting_challenge.fixtures.TestConstants.VALID_ID;
 
 public class VoteFixtures {
     public static VoteRequest.VoteRequestBuilder createValidVoteRequestBuilder() {
         return VoteRequest.builder()
+                .userId(VALID_ID)
                 .voteOption(VoteOption.YES);
     }
 
     public static VoteRequest.VoteRequestBuilder createInvalidVoteRequestBuilder() {
         return VoteRequest.builder()
+                .userId(INVALID_ID)
                 .voteOption(null);
     }
 
@@ -28,7 +31,7 @@ public class VoteFixtures {
     public static Vote.VoteBuilder createValidVoteEntityBuilder() {
         return Vote.builder()
                 .id(VALID_ID)
-                .user(VoterFixtures.createValidVotingUserEntity())
+                .user(UserFixtures.createValidUserEntity())
                 .agenda(AgendaFixtures.createAgenda())
                 .voteOption(VoteOption.YES);
     }
