4e2feae74d23f75c7e4604920dc99c4bb283a695
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-09-25 14:37:21 -0300

    chore: add logs to AgendaController for request/response tracking.

--- src/main/java/github/io/api_voting_challenge/controller/AgendaController.java
+++ src/main/java/github/io/api_voting_challenge/controller/AgendaController.java
@@ -6,12 +6,14 @@ import github.io.api_voting_challenge.dto.AgendaResponse;
 import github.io.api_voting_challenge.service.AgendaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/agendas")
@@ -21,31 +23,46 @@ public class AgendaController implements AgendaControllerDocs {
     @Override
     @PostMapping
     public ResponseEntity<AgendaResponse> create(@RequestBody @Valid AgendaRequest agendaRequest) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(agendaService.create(agendaRequest));
+        log.info("Received request to create agenda: {}", agendaRequest);
+        AgendaResponse agendaResponse = agendaService.create(agendaRequest);
+        log.info("Agenda created successfully: {}", agendaResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(agendaResponse);
     }
 
     @Override
     @PutMapping("/{id}")
     public ResponseEntity<AgendaResponse> update(@PathVariable Long id, @RequestBody @Valid AgendaRequest agendaRequest) {
-        return ResponseEntity.ok(agendaService.update(id, agendaRequest));
+        log.info("Received request to update agenda with id {}: {}", id, agendaRequest);
+        AgendaResponse agendaResponse = agendaService.update(id, agendaRequest);
+        log.info("Agenda updated successfully: {}", agendaResponse);
+        return ResponseEntity.ok(agendaResponse);
     }
 
     @Override
     @GetMapping("/{id}")
     public ResponseEntity<AgendaResponse> getById(@PathVariable Long id) {
-        return ResponseEntity.ok(agendaService.getById(id));
+        log.info("Received request to get agenda with id: {}", id);
+        AgendaResponse agendaResponse = agendaService.getById(id);
+        log.info("Agenda retrieved successfully: {}", agendaResponse);
+        return ResponseEntity.ok(agendaResponse);
     }
 
     @Override
     @GetMapping
     public ResponseEntity<Page<AgendaResponse>> getAll(Pageable pageable) {
-        return ResponseEntity.ok(agendaService.getAll(pageable));
+        log.info("Received request to get all agendas - page={}, size={}, sort={}",
+                pageable.getPageNumber(), pageable.getPageSize(), pageable.getSort());
+        Page<AgendaResponse> agendaResponse = agendaService.getAll(pageable);
+        log.info("Agendas retrieved successfully: {}", agendaResponse);
+        return ResponseEntity.ok(agendaResponse);
     }
 
     @Override
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
+        log.info("Received request to delete agenda with id: {}", id);
         agendaService.delete(id);
+        log.info("Agenda with id {} deleted successfully", id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
