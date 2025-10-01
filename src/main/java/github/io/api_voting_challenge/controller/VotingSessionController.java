a56486d90a77d63cc16e6d01408ebca2ee216fa7
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-10-01 19:27:11 -0300

    feat: implements logs in VotingSessionController.

--- src/main/java/github/io/api_voting_challenge/controller/VotingSessionController.java
+++ src/main/java/github/io/api_voting_challenge/controller/VotingSessionController.java
@@ -8,6 +8,7 @@ import github.io.api_voting_challenge.service.VoteService;
 import github.io.api_voting_challenge.service.VotingSessionScheduler;
 import github.io.api_voting_challenge.service.VotingSessionService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +16,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/voting-sessions")
@@ -26,19 +28,28 @@ public class VotingSessionController implements VotingSessionControllerDocs {
     @Override
     @PostMapping
     public ResponseEntity<VotingSessionResponse> create(@RequestBody VotingSessionRequest votingSessionRequest) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(votingSessionService.openVotingSession(votingSessionRequest));
+        log.info("Received request to open voting session: {}", votingSessionRequest.agendaId());
+        VotingSessionResponse votingSessionResponse = votingSessionService.openVotingSession(votingSessionRequest);
+        log.info("Voting session opened successfully: {}", votingSessionResponse.id());
+        return ResponseEntity.status(HttpStatus.CREATED).body(votingSessionResponse);
     }
 
     @Override
     @GetMapping
     public ResponseEntity<Page<VotingSessionResponse>> getOpenVotingSessions(@ParameterObject Pageable pageable) {
+        log.info("Received request to get all open voting sessions - page={}, size={}, sort={}",
+                pageable.getPageNumber(), pageable.getPageSize(), pageable.getSort());
         Page<VotingSessionResponse> openSessions = votingSessionScheduler.getOpenVotingSessions(pageable);
+        log.info("Open voting sessions retrieved successfully: {}", openSessions.getTotalElements());
         return ResponseEntity.ok(openSessions);
     }
 
     @Override
     @GetMapping("/{sessionId}/results")
     public ResponseEntity<VoteResultResponse> getVotingResults(@PathVariable Long sessionId) {
-        return ResponseEntity.ok(voteService.calculateVotingResult(sessionId));
+        log.info("Received request to get voting results for session id: {}", sessionId);
+        VoteResultResponse resultResponse = voteService.calculateVotingResult(sessionId);
+        log.info("Voting results retrieved successfully for session id: {}", sessionId);
+        return ResponseEntity.ok(resultResponse);
     }
 }
