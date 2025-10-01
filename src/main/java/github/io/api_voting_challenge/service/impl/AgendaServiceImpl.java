c20ff487129de56db14293e020e26008f20f6079
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-10-01 19:35:27 -0300

    feat: implements logs in AgendaServiceImpl.

--- src/main/java/github/io/api_voting_challenge/service/impl/AgendaServiceImpl.java
+++ src/main/java/github/io/api_voting_challenge/service/impl/AgendaServiceImpl.java
@@ -10,6 +10,7 @@ import github.io.api_voting_challenge.repository.AgendaRepository;
 import github.io.api_voting_challenge.service.AgendaService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -19,12 +20,14 @@ import java.time.LocalDate;
 @Service
 @Transactional
 @RequiredArgsConstructor
+@Slf4j
 public class AgendaServiceImpl implements AgendaService {
     private final AgendaRepository agendaRepository;
     private final AgendaMapper agendaMapper;
 
     @Override
     public AgendaResponse create(AgendaRequest agendaRequest) {
+        log.info("Creating new agenda with title: {}", agendaRequest.title());
         Agenda agendaToSave = Agenda.builder()
                 .title(agendaRequest.title())
                 .description(agendaRequest.description())
@@ -32,11 +35,15 @@ public class AgendaServiceImpl implements AgendaService {
                 .status(Status.PENDING)
                 .build();
 
-        return agendaMapper.toDto(agendaRepository.save(agendaToSave));
+        Agenda agenda = agendaRepository.save(agendaToSave);
+        log.info("Agenda created with ID: {}", agenda.getId());
+
+        return agendaMapper.toDto(agenda);
     }
 
     @Override
     public AgendaResponse update(Long id, AgendaRequest agendaRequest) {
+        log.info("Updating agenda with ID: {}", id);
         Agenda existingAgenda = agendaRepository.findById(id).orElseThrow(
                 () -> new AgendaNotFoundException("Agenda not found with ID: " + id)
         );
@@ -44,21 +51,37 @@ public class AgendaServiceImpl implements AgendaService {
         existingAgenda.setTitle(agendaRequest.title());
         existingAgenda.setDescription(agendaRequest.description());
 
-        return agendaMapper.toDto(agendaRepository.save(existingAgenda));
+        Agenda agenda = agendaRepository.save(existingAgenda);
+        log.info("Agenda with ID: {} updated successfully.", agenda.getId());
+        
+        return agendaMapper.toDto(agenda);
     }
 
     @Override
     public AgendaResponse getById(Long id) {
-        return agendaMapper.toDto(getAgenda(id));
+        log.info("Retrieving agenda with ID: {}", id);
+        Agenda agenda = getAgenda(id);
+        log.info("Agenda with ID: {} retrieved successfully.", agenda.getId());
+        
+        return agendaMapper.toDto(agenda);
     }
 
-    public Page<AgendaResponse> getAll(Pageable pageable){
-        return agendaRepository.findAll(pageable).map(agendaMapper::toDto);
+    public Page<AgendaResponse> getAll(Pageable pageable) {
+        log.info("Retrieving all agendas - page: {}, size: {}, sort: {}",
+                pageable.getPageNumber(), pageable.getPageSize(), pageable.getSort());
+        Page<Agenda> allAgendas = agendaRepository.findAll(pageable);
+        log.info("Total agendas retrieved: {}", allAgendas.getTotalElements());
+
+        return allAgendas.map(agendaMapper::toDto);
     }
 
     @Override
     public void delete(Long id) {
-        agendaRepository.delete(getAgenda(id));
+        log.info("Deleting agenda with ID: {}", id);
+        Agenda agenda = getAgenda(id);
+
+        agendaRepository.delete(agenda);
+        log.info("Agenda with ID: {} deleted successfully.", id);
     }
 
     private Agenda getAgenda(Long id) {
