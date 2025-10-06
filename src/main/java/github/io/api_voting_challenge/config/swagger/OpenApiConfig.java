e2a6b0db98301492ac90bf0b6b76fc34dd800406
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-10-06 14:44:09 -0300

    refactor: move the swagger configuration class to the config/swagger folder because another config is created and thus separates the responsibilities.

--- src/main/java/github/io/api_voting_challenge/config/swagger/OpenApiConfig.java
+++ src/main/java/github/io/api_voting_challenge/config/swagger/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package github.io.api_voting_challenge.config;
+package github.io.api_voting_challenge.config.swagger;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
