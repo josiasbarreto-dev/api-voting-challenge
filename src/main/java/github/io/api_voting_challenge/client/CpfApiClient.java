ca690b3568a18756a0f927e69a82bdf4a397a0c4
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-10-08 12:42:26 -0300

    feat: creates interface with client configuration.

---
+++ src/main/java/github/io/api_voting_challenge/client/CpfApiClient.java
@@ -0,0 +1,16 @@
+package github.io.api_voting_challenge.client;
+
+import github.io.api_voting_challenge.client.response.CpfStatusResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "CPFValidatorClient",
+        url = "${external.cpf-api.url}",
+        path = "/api/v1"
+)
+public interface CpfApiClient {
+    @GetMapping("/users/{cpf}")
+    CpfStatusResponse validateCpf(@PathVariable("cpf") String cpf);
+}
