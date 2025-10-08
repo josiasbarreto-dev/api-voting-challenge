a78e022d0374e1fe72d0ee6ac00593531a4c9305
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-10-08 20:53:20 -0300

    refactor: change the return of the interface method to return a ResponseEntity<> and thus be able to obtain the getStatusCode() and work on the status code and not a fixed string.

--- src/main/java/github/io/api_voting_challenge/client/CpfApiClient.java
+++ src/main/java/github/io/api_voting_challenge/client/CpfApiClient.java
@@ -2,6 +2,7 @@ package github.io.api_voting_challenge.client;
 
 import github.io.api_voting_challenge.client.response.CpfStatusResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -12,5 +13,5 @@ import org.springframework.web.bind.annotation.PathVariable;
 )
 public interface CpfApiClient {
     @GetMapping("/users/{cpf}")
-    CpfStatusResponse validateCpf(@PathVariable("cpf") String cpf);
+    ResponseEntity<CpfStatusResponse> validateCpf(@PathVariable("cpf") String cpf);
 }
