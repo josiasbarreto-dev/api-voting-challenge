d9335172c255fcceac15ea4feb77cdebc25db0a3
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-10-08 12:27:24 -0300

    feat: add 'EnableFeignClients' annotation to allow this application to consume other.

--- src/main/java/github/io/api_voting_challenge/Application.java
+++ src/main/java/github/io/api_voting_challenge/Application.java
@@ -2,10 +2,12 @@ package github.io.api_voting_challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableFeignClients
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
