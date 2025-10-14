987a1452cef5da1f10abdcd8e5fc21e21e691c47
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-10-14 07:13:45 -0300

    feat: adds annotation that enables caching in the application.

--- src/main/java/github/io/api_voting_challenge/Application.java
+++ src/main/java/github/io/api_voting_challenge/Application.java
@@ -2,12 +2,14 @@ package github.io.api_voting_challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients
+@EnableCaching
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
