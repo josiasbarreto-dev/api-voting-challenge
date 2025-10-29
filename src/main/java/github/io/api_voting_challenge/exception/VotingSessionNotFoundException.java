2a2db35c74745ffdbcf26d7f8bdef04ae8a65dc1
Josias Barreto <sr.josiasbarreto@gmail.com>
2025-10-29 11:22:12 -0300

    refactor: replaces AgendaNotFoundException with BusinessException for improved error handling in AgendaController unit tests

--- src/main/java/github/io/api_voting_challenge/exception/VotingSessionNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package github.io.api_voting_challenge.exception;
-
-public class VotingSessionNotFoundException extends RuntimeException{
-    public VotingSessionNotFoundException(String message) {
-        super(message);
-    }
-}
